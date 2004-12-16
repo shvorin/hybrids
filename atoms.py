fa85ac1706fcfc147978becbe284ef4c5e21529b
Artem Shvorin <art@shvorin.net>
2004-12-16 07:52:02 +0000

    various experiments

--- atoms.py
+++ atoms.py
@@ -1,35 +1,98 @@
-
 __Id__ = "$Id$"
 
+import new
 
-class Atoms:
-    count = 0
+class Immutable(object):
+    __slots__ = ()
+
+    def __init__(self, *args):
+        raise TypeError, "cannot create 'Immutable' instances, this is an abstract class"
     
-    def __init__(self, scope):
-        self.scope = scope
+    def __setattr__(self, name, value):
+        raise TypeError, "readonly attribute"
 
-    def newAtom(self, name):
-        if getattr(self.scope, name, None) != None:
-            # FIXME: is it OK to use standard exception 'AttributeError' ?
-            raise AttributeError, ("symbol '%s.%s' already defined" % (self.scope, name))
-        setattr(self.scope, name, Atoms.count)
-        Atoms.count = Atoms.count+1
+    def __delattr__(self, name):
+        raise TypeError, "readonly attribute"
 
-    def newAtoms(self, names):
-        for name in names: self.newAtom(name)
+    def __cmp__(self, other):
+        return cmp(self(), other())
+
+    def __hash__(self):
+        return hash(self())
+
+    def __call__(self):
+        return tuple(map((lambda name: object.__getattribute__(self, name)), self.__slots__))
+ 
+
+class Color(Immutable):
+    __slots__ = ('value', )
+    __white = 0
+    __black = 1
+    
+    def __init__(self, *args):
+        raise TypeError, "cannot create 'Color' instances"
+
+    # FIXME: is it OK to use __repr__(), not just __str__() ?
+    def __repr__(self):
+        if   self.value == Color.__white: return 'white'
+        elif self.value == Color.__black: return 'black'
+        else : raise Exception, "oops, unknown color"
 
 
-def getCount():
-        return Atoms.count
+    # to be deleted
+    def createValues(self):
+        v1 = object.__new__(Color)
+        object.__setattr__(v1, 'value', Color.__white)
+        v2 = object.__new__(Color)
+        object.__setattr__(v2, 'value', Color.__black)
+        return (v1, v2)
 
-def newAtom(name):
-    "updates globals"
-    g = globals()
-    if g.has_key(name):
-        raise ("global symbol '%s' already defined" % name)
-    g[name] = Atoms.count
-    Atoms.count = Atoms.count + 1
+    def inv(self):
+        if self == white: return black
+        else: return white
 
-def newAtoms(names):
-    "updates globals"
-    for name in names: newAtom(name)
+(white, black) = Color.createValues(object.__new__(Color))
+del Color.createValues # no more Color values ;)
+
+class Loc(Immutable):
+    __slots__ = (x, y)
+    
+    __files = 'abcdefgh'
+    __ranks = '12345678'
+
+    def __init__(self, *args):
+        length = len(args)
+        if length == 2:
+            (x, y) = args
+        elif length == 1:
+            ((x, y), ) = args
+        else:
+            raise ValueError, "wrong # of args"
+
+        if x in list(Loc.__files) and y in list(Loc.__ranks):
+            # algebraic notation given
+            try:
+                x, y = Loc.__files.index(x), Loc.__ranks.index(y)
+            except ValueError:
+                raise ValueError, "invalid algebraic notation"
+        else:
+            # numeric representation given
+            if not (x in range(8) and y in range(8)):
+                raise ValueError, "numeric representation is out of range"
+        
+        # explicit assignment forbidden
+        self.__dict__['x'] = x
+        self.__dict__['y'] = y
+            
+
+    # FIXME: is it OK to use __repr__(), not just __str__() ?
+    def __repr__(self):
+        return Loc.__files[self.x] + Loc.__ranks[self.y]
+    
+#     def __call__(self):
+#         return self.x, self.y
+
+    def flip(self):
+        return Loc(self.x, 7-self.y)
+    
+del new
