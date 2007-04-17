0d588b95c1cede2879d6b24a9e7c77463c82c7e5
Artem Shvorin <art@shvorin.net>
2007-04-17 08:14:03 +0000

    1. подправлен PGN-парсер (пока не используется)  2. режим "edit game": не ломается в случае окончания игры -- можно откатить историю назад и редактировать дальше

--- PGN.py
+++ PGN.py
@@ -16,46 +16,52 @@ Prime          := [GNBR]
 Hybrid         := Prime, Prime
 King           := [K]
 Pawn           := [pP]?
->Piece<        := Hybrid/Piece/King/Pawn
+>Piece<        := Hybrid/Prime/King
 Hybridable     := Prime
 NotHybridable  := Hybrid/King
 
-CheckSign      := [+\#]
+CheckSign      := [+#]
 
 CaptureSign    := [x:]
 JoinSign       := [^]
 PassSign       := [-]
 >MoveSign<     := PassSign/CaptureSign/JoinSign
 
-Promotion      := [=], Prime
+Promotion      := [=]?, Prime
 
 # Full Algebraic notation
 
->Move_FAN_NotHybridable< := NotHybridable, Loc, (CaptureSign/PassSign)?, Loc, CheckSign?
->Move_FAN_Hybridable<    := Hybridable, Loc, MoveSign?, Loc, CheckSign?
->Move_FAN_Pawn<          := Hybridable, Loc, (CaptureSign/PassSign)?, Loc, Promotion?, CheckSign?
+>Move_FAN_NotHybridable< := NotHybridable, Loc, (CaptureSign/PassSign)?, Loc
+>Move_FAN_Hybridable<    := Hybridable, Loc, MoveSign?, Loc
+>Move_FAN_Pawn<          := Pawn, Loc, (CaptureSign/PassSign)?, Loc, Promotion?
 
 # strict version
-Move_FAN       := Move_FAN_NotHybridable/Move_FAN_Hybridable/Move_FAN_Pawn
+Move_FAN       := (Move_FAN_NotHybridable/Move_FAN_Hybridable/Move_FAN_Pawn), CheckSign?
 
-# relaxed version
-# Move_FAN       := Piece, Loc, MoveSign?, Loc, Promotion?, CheckSign?
 
 # Short Algebraic notation
->move_aux<     := MoveSign?, Loc, Promotion?, CheckSign?
 
-Move_SAN       := Piece, (((File/Rank), move_aux)/move_aux)
+>Move_SAN_Pawn_capture<  := Pawn, (File, CaptureSign?, Loc)/(File, CaptureSign?, File)
+>Move_SAN_Pawn<          := (Pawn, Loc)/Move_SAN_Pawn_capture, Promotion?
+>Move_SAN_NotHybridable< := NotHybridable, (File/Rank, MoveSign?, Loc) / (MoveSign?, Loc)
+>Move_SAN_Hybridable<    := Hybridable, (File/Rank, (PassSign/CaptureSign)?, Loc) / ((PassSign/CaptureSign)?, Loc)
+
+Move_SAN                 := (Move_SAN_NotHybridable/Move_SAN_Hybridable/Move_SAN_Pawn), CheckSign?
 
 # Allow any
-Move           := Move_SAN/Move_FAN
+Move           := Move_FAN/Move_SAN
 '''
 
 parser = Parser(move)
 
 tags = r'''
+<space>        := [ \t\n\r]+
 <sspace>       := [ \t]+
 Symbol         := [a-zA-Z0-9_]+
 StringToken    := '"', -('"')*, '"'
-Tag            := ("[", Symbol, sspace, StringToken, "]") !
->Tags<         := Tag*
+#Tag            := ("[", Symbol, sspace, StringToken, "]") !
+Tag            := ("[", Symbol, sspace, StringToken, "]")
+>Tags<         := (Tag, space+)*
 '''
+
+p1 = Parser(tags)
