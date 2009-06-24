9531286988bc1a25666b0da4ca57337124e55fca
Artem Shvorin <art@shvorin.net>
2009-06-24 16:41:32 +0000

    PGN: дописано парсирование всего PGN, включая теги. Весь EBNF-код перенесен в специальную директорию.

--- PGN.py
+++ PGN.py
@@ -3,65 +3,10 @@ __Id__ = "$Id$"
 from simpleparse.common import numbers, strings, comments
 from simpleparse.parser import Parser
 from simpleparse.dispatchprocessor import *
+import os.path
 
-move = r'''# note use of raw string when embedding in python code...
-<space>        := [ \t\n\r]+
-<sspace>        := [ \t]+
+def load_ebnf(fname):
+    # FIXME: a path to EBNF is hard-coded
+    return file(os.path.join('EBNF', fname+'.ebnf')).read()
 
-File           := [a-h]
-Rank           := [1-8]
-Loc            := File, Rank
-
-Prime          := [GNBR]
-Hybrid         := Prime, Prime
-King           := [K]
-Pawn           := [pP]?
->Piece<        := Hybrid/Prime/King
-Hybridable     := Prime
-NotHybridable  := Hybrid/King
-
-CheckSign      := [+#]
-
-CaptureSign    := [x:]
-JoinSign       := [^]
-PassSign       := [-]
->MoveSign<     := PassSign/CaptureSign/JoinSign
-
-Promotion      := [=]?, Prime
-
-# Full Algebraic notation
-
->Move_FAN_NotHybridable< := NotHybridable, Loc, (CaptureSign/PassSign)?, Loc
->Move_FAN_Hybridable<    := Hybridable, Loc, MoveSign?, Loc
->Move_FAN_Pawn<          := Pawn, Loc, (CaptureSign/PassSign)?, Loc, Promotion?
-
-# strict version
-Move_FAN       := (Move_FAN_NotHybridable/Move_FAN_Hybridable/Move_FAN_Pawn), CheckSign?
-
-
-# Short Algebraic notation
-
->Move_SAN_Pawn_capture<  := Pawn, (File, CaptureSign?, Loc)/(File, CaptureSign?, File)
->Move_SAN_Pawn<          := (Pawn, Loc)/Move_SAN_Pawn_capture, Promotion?
->Move_SAN_NotHybridable< := NotHybridable, (File/Rank, MoveSign?, Loc) / (MoveSign?, Loc)
->Move_SAN_Hybridable<    := Hybridable, (File/Rank, (PassSign/CaptureSign)?, Loc) / ((PassSign/CaptureSign)?, Loc)
-
-Move_SAN                 := (Move_SAN_NotHybridable/Move_SAN_Hybridable/Move_SAN_Pawn), CheckSign?
-
-# Allow any
-Move           := Move_FAN/Move_SAN
-'''
-
-parser = Parser(move)
-
-tags = r'''
-<space>        := [ \t\n\r]+
-<sspace>       := [ \t]+
-Symbol         := [a-zA-Z0-9_]+
-StringToken    := '"', -('"')*, '"'
-#Tag            := ("[", Symbol, sspace, StringToken, "]") !
-Tag            := ("[", Symbol, sspace, StringToken, "]")
->Tags<         := (Tag, space+)*
-'''
-
-p1 = Parser(tags)
+parser = Parser(load_ebnf('move') + load_ebnf('tags') + load_ebnf('game'))
